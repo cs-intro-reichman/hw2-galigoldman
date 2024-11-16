3c52778f4964a82cd2f2577da4cdfcb572acd712
galigoldman <gali.goldman@post.runi.ac.il>
2024-11-16 12:30:36 +0200

    homework 2

--- Cheers.java
+++ Cheers.java
@@ -2,5 +2,36 @@
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+            String str=args[0];
+            int length = str.length(), i=0, times=Integer.parseInt(args[1]);
+            String[] letters = str.split("");
+            for(i=0;i<length;i++)
+            {
+                switch (letters[i]) {
+                        case "A":
+                        case "E":
+                        case "F":
+                        case "H":
+                        case "I":
+                        case "L":
+                        case "M":
+                        case "N":
+                        case "O":
+                        case "R":
+                        case "S":
+                        case "X":
+                                System.out.println("Give Me an "+letters[i] + "  :" + letters[i] + "!");
+                                break;
+                
+                        default:
+                                System.out.println("Give Me a "+letters[i] + "  :" + letters[i] + "!");
+                                break;
+                }
+            }
+            System.out.println("What does that spell?");
+            for(i=0;i<times;i++)
+            {
+                System.out.println(str+"!!!");
+            }
         }
 }
--- Collatz.java
+++ Collatz.java
@@ -2,5 +2,29 @@
 public class Collatz {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int n = Integer.parseInt(args[0]);
+		String str = args[1];
+		int i=0,seed=0,count=1;
+		for(i=1;i<=n;i++){
+			seed=i;
+			while(seed!=1||count==1){
+				count++;
+				if(str.equals("v")){
+					System.out.print(seed + " ");
+				}
+				if(seed%2==0){
+					seed = seed/2;
+				}
+				else{
+					seed = seed * 3 + 1;
+				}
+			}
+			if(str.equals("v")){
+			System.out.print("1");
+			System.out.println(" (" +count + ")");
+			count=1;
+			}
+		}
+		System.out.println("Every one of the first " + n +" hailstone sequences reached 1.");
 	}
 }
