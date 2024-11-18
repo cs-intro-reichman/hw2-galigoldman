24df6bcc48533a3c203e344deda122d4279f4570
galigoldman <gali.goldman@post.runi.ac.il>
2024-11-18 10:13:51 +0200

    fix

--- Cheers.java
+++ Cheers.java
@@ -3,6 +3,7 @@ public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
             String str=args[0];
+            str = str.toUpperCase();
             int length = str.length(), i=0, times=Integer.parseInt(args[1]);
             String[] letters = str.split("");
             for(i=0;i<length;i++)
@@ -20,18 +21,6 @@ public class Cheers {
                         case "R":
                         case "S":
                         case "X":
-                        case "a":
-                        case "e":
-                        case "f":
-                        case "h":
-                        case "i":
-                        case "l":
-                        case "m":
-                        case "n":
-                        case "o":
-                        case "r":
-                        case "s":
-                        case "x":
                                 System.out.println("Give me an "+letters[i] + ": " + letters[i] + "!");
                                 break;
                 
@@ -43,7 +32,7 @@ public class Cheers {
             System.out.println("What does that spell?");
             for(i=0;i<times;i++)
             {
-                System.out.println(str.toUpperCase()+"!!!");
+                System.out.println(str+"!!!");
             }
         }
 }
